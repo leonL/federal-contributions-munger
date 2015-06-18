2a4d1613b05101e9bd5d3630def4a7eb9d96df43
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2015-06-18 13:45:32 -0400

    minor fix

--- tests/test_LIB-base.R
+++ tests/test_LIB-base.R
@@ -1,4 +1,4 @@
-source('../R/lib/munger.R', chdir=TRUE)
+source('../R/lib/base.R', chdir=TRUE)
 
 test_that("An error is thrown when an inline test fails", {
   expect_error(test$Base(FALSE, "error message"), "error message")
